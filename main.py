aad70dbc717a0a254e14728b4459ff5ddb41d2e4
Alex Muirhead <a.muirhead@uq.net.au>
2025-10-31 15:30:51 +1000

    Use correct num_cells, add neighbours

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 
 REPO_ROOT = Path("/home/alex/GDTk/gdtk.robust-python-modules")
 MAX_VERTICES = 4
+SENTINAL = -1
 
 
 def main():
@@ -13,21 +14,39 @@ def main():
     grid = snap.grids[0]
 
     # Build everything into a single array
-    num_cells = grid.niv * grid.njv
+    nic = grid.niv - 1
+    njc = grid.njv - 1
+    num_cells = nic * njc
+    num_verts = grid.niv * grid.njv
 
     vertices = np.stack((grid.vertices.x, grid.vertices.y, grid.vertices.z), axis=-1)
-    vertices = vertices.reshape((num_cells, 3))
+    vertices = vertices.reshape((num_verts, 3))
     print(f"Vertices shape: {vertices.shape}")
 
     cells = np.zeros((num_cells, MAX_VERTICES), dtype=int)
+    neighbours = np.zeros((num_cells, MAX_VERTICES), dtype=int)
 
-    for j in range(grid.njv - 1):
-        for i in range(grid.niv - 1):
-            idx = j * grid.niv + i
+    # This is all trivial to setup for a structured grid
+    for j in range(nic):
+        for i in range(njc):
+            cell_idx = j * nic + i
             # Anti-clockwise, starting from bottom-left
-            cells[idx, :] = [idx, idx + 1, idx + grid.niv + 1, idx + grid.niv]
+            cells[cell_idx, :] = [
+                cell_idx,
+                cell_idx + 1,
+                cell_idx + grid.niv + 1,
+                cell_idx + grid.niv,
+            ]
+            # Anti-clockwise, starting from 0rad (EAST)
+            neighbours[cell_idx, :] = [
+                cell_idx + 1 if i < nic else SENTINAL,
+                cell_idx + nic if j < njc else SENTINAL,
+                cell_idx - 1 if i > 0 else SENTINAL,
+                cell_idx - nic if j > 0 else SENTINAL,
+            ]
 
-    print(cells)
+    print(f"Cell definition: shape = {cells.shape}")
+    print(f"Cell neighbours: shape = {neighbours.shape}")
 
 
 if __name__ == "__main__":
