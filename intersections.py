ff0275303057f759ebe513bb1b1ad29ce2517751
Alex Muirhead <a.muirhead@uq.net.au>
2026-01-08 14:13:23 +1000

    Create 3D tests for containment

--- intersections.py
+++ intersections.py
@@ -7,7 +7,7 @@ from jaxtyping import Array, Float
 
 class ConvexCell(eqx.Module):
     normals: Float[Array, "nfaces ndim"]
-    offsets: Float[Array, "nfaces 1"]
+    offsets: Float[Array, " nfaces"]
 
     @jax.jit(static_argnames="epsilon")
     def contains(self, points: Float[Array, "npoints ndim"], epsilon: float = 0.0):
@@ -20,7 +20,7 @@ class ConvexCell(eqx.Module):
 # ================================ UNITTESTS ================================
 
 
-def unit_cell() -> ConvexCell:
+def unit_2D_cell() -> ConvexCell:
     """Generate a unit-square cell."""
     # fmt: off
     normals = np.array(
@@ -38,7 +38,27 @@ def unit_cell() -> ConvexCell:
     return ConvexCell(normals=normals, offsets=offsets)
 
 
-def random_quad() -> ConvexCell:
+def unit_3D_cell() -> ConvexCell:
+    """Generate a unit-cube cell."""
+    # fmt: off
+    normals = np.array(
+        [[+1.0,  0.0,  0.0],
+         [ 0.0, +1.0,  0.0],
+         [ 0.0,  0.0, +1.0],
+         [-1.0,  0.0,  0.0],
+         [ 0.0, -1.0,  0.0],
+         [ 0.0,  0.0, -1.0]],
+        dtype=float
+    )
+    offsets = np.array(
+        [1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+        dtype=float
+    )
+    # fmt: on
+    return ConvexCell(normals=normals, offsets=offsets)
+
+
+def random_2D_quad() -> ConvexCell:
     # fmt: off
     vertices = np.array(
         [[0.0, 0.0],
@@ -66,7 +86,7 @@ def print_log(log: dict[str, object]) -> None:
         print("")
 
 
-def test_containment_simple():
+def test_2D_containment_simple():
     # fmt: off
     points = np.array(
         [[0.5 , 0.5 ],
@@ -76,24 +96,59 @@ def test_containment_simple():
         dtype=float
     )
     # fmt: on
-    cell = unit_cell()
+    cell = unit_2D_cell()
     actual = cell.contains(points)
     expected = np.array([True, True, True, False])
 
     assert np.all(actual == expected)
 
 
-def test_containment_edges():
+def test_3D_containment_simple():
     # fmt: off
     points = np.array(
-        [[1.0, 1.0],
-         [0.0, 0.0],
-         [0.5, 1.0],
+        [[0.5 , 0.5 , 0.5 ],
+         [0.25, 0.9 , 0.1 ],
+         [0.1 , 0.75, 0.75],
+         [ 1.5, 1.5 , 1.5 ]],
+        dtype=float
+    )
+    # fmt: on
+    cell = unit_3D_cell()
+    actual = cell.contains(points)
+    expected = np.array([True, True, True, False])
+
+    assert np.all(actual == expected)
+
+
+def test_2D_containment_edges():
+    # fmt: off
+    points = np.array(
+        [[ 1.0 ,  1.0 ],
+         [ 0.0 ,  0.0 ],
+         [ 0.5 ,  1.0 ],
          [-1E-6, -1E-6]],
         dtype=float
     )
     # fmt: on
-    cell = unit_cell()
+    cell = unit_2D_cell()
+
+    actual = cell.contains(points, epsilon=0.001)
+    expected = np.array([True, True, True, True])
+
+    assert np.all(actual == expected)
+
+
+def test_3D_containment_edges():
+    # fmt: off
+    points = np.array(
+        [[ 1.0 ,  1.0 ,  1.0 ],
+         [ 0.0 ,  0.0 ,  0.0 ],
+         [ 0.5 ,  1.0 ,  0.5 ],
+         [-1E-6, -1E-6, -1E-6]],
+        dtype=float
+    )
+    # fmt: on
+    cell = unit_3D_cell()
 
     actual = cell.contains(points, epsilon=0.001)
     expected = np.array([True, True, True, True])
